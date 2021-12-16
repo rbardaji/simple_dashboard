593d0ecb8f5c043710ca7b9897b926e35278a357
Raul Bardaji <rbardaji@gmail.com>
2021-12-16 22:54:19 +0100

    He añadido todos los parameter pie de Antares

--- autoreload.js
+++ autoreload.js
@@ -1,1 +1,22 @@
-setTimeout("location.reload(true);", 600000);
+window.setInterval(function() {
+    reloadIFrame()
+}, 3000);
+
+function reloadIFrame() {
+    console.log('reloading..');
+    document.getElementById('Monthly').contentWindow.location.reload(true);
+    document.getElementById('15_Days').contentWindow.location.reload(true);
+    document.getElementById('10_Days').contentWindow.location.reload(true);
+    document.getElementById('6_Days').contentWindow.location.reload(true);
+    document.getElementById('5_Days').contentWindow.location.reload(true);
+    document.getElementById('4_Days').contentWindow.location.reload(true);
+    document.getElementById('3_Days').contentWindow.location.reload(true);
+    document.getElementById('2_Days').contentWindow.location.reload(true);
+    document.getElementById('Daily').contentWindow.location.reload(true);
+    document.getElementById('12_Hours').contentWindow.location.reload(true);
+    document.getElementById('8_Hours').contentWindow.location.reload(true);
+    document.getElementById('6_Hours').contentWindow.location.reload(true);
+    document.getElementById('3_Hours').contentWindow.location.reload(true);
+    document.getElementById('2_Hours').contentWindow.location.reload(true);
+    document.getElementById('Hourly').contentWindow.location.reload(true);
+}
--- create_plots.js
+++ create_plots.js
@@ -1,4 +1,14 @@
 var request_platform_aviability = new XMLHttpRequest()
+var request_parameter_pie_H = new XMLHttpRequest()
+var request_parameter_pie_2H = new XMLHttpRequest()
+var request_parameter_pie_3H = new XMLHttpRequest()
+var request_parameter_pie_6H = new XMLHttpRequest()
+var request_parameter_pie_8H = new XMLHttpRequest()
+var request_parameter_pie_12H = new XMLHttpRequest()
+var request_parameter_pie_D = new XMLHttpRequest()
+var request_parameter_pie_2D = new XMLHttpRequest()
+var request_parameter_pie_3D = new XMLHttpRequest()
+var request_parameter_pie_4D = new XMLHttpRequest()
 var request_parameter_pie_5D = new XMLHttpRequest()
 var request_parameter_pie_6D = new XMLHttpRequest()
 var request_parameter_pie_10D = new XMLHttpRequest()
@@ -64,3 +74,113 @@ request_parameter_pie_5D.onload = function () {
     }
 }
 request_parameter_pie_5D.send()
+
+request_parameter_pie_4D.open('GET', 'http://localhost:8050/fig/parameter_pie/4D?platform_code=ANTARES', true)
+request_parameter_pie_4D.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 4D');
+    } else {
+        console.log('error in call parameter_pie 5D')
+    }
+}
+request_parameter_pie_4D.send()
+
+request_parameter_pie_3D.open('GET', 'http://localhost:8050/fig/parameter_pie/3D?platform_code=ANTARES', true)
+request_parameter_pie_3D.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 3D');
+    } else {
+        console.log('error in call parameter_pie 3D')
+    }
+}
+request_parameter_pie_3D.send()
+
+request_parameter_pie_2D.open('GET', 'http://localhost:8050/fig/parameter_pie/2D?platform_code=ANTARES', true)
+request_parameter_pie_2D.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 2D');
+    } else {
+        console.log('error in call parameter_pie 2D')
+    }
+}
+request_parameter_pie_2D.send()
+
+request_parameter_pie_D.open('GET', 'http://localhost:8050/fig/parameter_pie/D?platform_code=ANTARES', true)
+request_parameter_pie_D.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie D');
+    } else {
+        console.log('error in call parameter_pie D')
+    }
+}
+request_parameter_pie_D.send()
+
+request_parameter_pie_12H.open('GET', 'http://localhost:8050/fig/parameter_pie/12H?platform_code=ANTARES', true)
+request_parameter_pie_12H.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 12H');
+    } else {
+        console.log('error in call parameter_pie 12H')
+    }
+}
+request_parameter_pie_12H.send()
+
+request_parameter_pie_8H.open('GET', 'http://localhost:8050/fig/parameter_pie/8H?platform_code=ANTARES', true)
+request_parameter_pie_8H.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 8H');
+    } else {
+        console.log('error in call parameter_pie 8H')
+    }
+}
+request_parameter_pie_8H.send()
+
+request_parameter_pie_6H.open('GET', 'http://localhost:8050/fig/parameter_pie/6H?platform_code=ANTARES', true)
+request_parameter_pie_6H.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 6H');
+    } else {
+        console.log('error in call parameter_pie 6H')
+    }
+}
+request_parameter_pie_6H.send()
+
+request_parameter_pie_3H.open('GET', 'http://localhost:8050/fig/parameter_pie/3H?platform_code=ANTARES', true)
+request_parameter_pie_3H.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 3H');
+    } else {
+        console.log('error in call parameter_pie 3H')
+    }
+}
+request_parameter_pie_3H.send()
+
+request_parameter_pie_2H.open('GET', 'http://localhost:8050/fig/parameter_pie/2H?platform_code=ANTARES', true)
+request_parameter_pie_2H.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie 2H');
+    } else {
+        console.log('error in call parameter_pie 2H')
+    }
+}
+request_parameter_pie_2H.send()
+
+request_parameter_pie_H.open('GET', 'http://localhost:8050/fig/parameter_pie/H?platform_code=ANTARES', true)
+request_parameter_pie_H.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie H');
+    } else {
+        console.log('error in call parameter_pie H')
+    }
+}
+request_parameter_pie_H.send()
+
+request_parameter_pie_R.open('GET', 'http://localhost:8050/fig/parameter_pie/R?platform_code=ANTARES', true)
+request_parameter_pie_R.onload = function () {
+    if (request.status >= 200 && request.status < 400) {
+        console.log('call parameter_pie R');
+    } else {
+        console.log('error in call parameter_pie R')
+    }
+}
+request_parameter_pie_R.send()
